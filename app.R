5dc0d3de25b637dcd1a34ac76be7dcff648c36bf
tomsaunders98 <tomandthenews@gmail.com>
2020-04-21 00:00:20 +0100

    App

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
 library(Cairo)
@@ -154,8 +145,7 @@ server <- function(input, output) {
 
     output$distPlot <- renderPlot({
         BigC$plot
-        
-        # generate bins based on input$bins from ui.R
+     
     })
 }
 
